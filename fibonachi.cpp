58f114235785ffb8b027661727bfefdecb64e693
obrpasha <obrpasha@gmail.com>
2021-04-21 12:23:49 +0300

    init version

---
+++ fibonachi.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <functional>
+
+
+class Fibonachi
+{
+public:
+  unsigned GetValue(unsigned n)
+  {
+      if (m_FibCache.size() <= n)
+      {
+          for(unsigned int i = m_FibCache.size() - 1; i < n; ++i)
+          {
+              const unsigned res = m_FibCache[m_FibCache.size() - 2] + m_FibCache[m_FibCache.size() - 1];
+              m_FibCache.push_back(res);
+          }
+
+          return m_FibCache.back();
+      }
+      else
+      {
+          return m_FibCache[n];
+      }
+  }
+
+private:
+   static std::vector<unsigned int> m_FibCache;
+};
+
+std::vector<unsigned int> Fibonachi::m_FibCache(1, 1);
+
+unsigned f1(unsigned n) {
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+  return f1(n - 1) + f1(n - 2);
+}
+
+Fibonachi f;
+
+unsigned int f2(unsigned n)
+{
+    return f.GetValue(n);
+}
+
+unsigned f3(unsigned n)
+{
+  if (n == 0 || n == 1)
+    return 1;
+
+  unsigned prevPrev = 1; //0
+  unsigned prev = 1; //0;
+
+  unsigned res = 0;
+  for (unsigned int i = 1; i<n; ++i)
+  {
+    res = prevPrev+prev;
+
+    prevPrev = prev;
+    prev = res;
+  }
+
+  return res;
+}
+
+void runner(unsigned n, std::function<unsigned(unsigned)> fibFn)
+{
+    auto startT = std::chrono::high_resolution_clock::now();
+
+    for (unsigned i = 0; i < n; i++)
+    {
+        const unsigned fibN = rand() % n;
+        unsigned res = fibFn(fibN);
+        //std::cout << "Fib(" << i << ")" << res  << std::endl;
+    }
+
+    auto endT = std::chrono::high_resolution_clock::now();
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(endT - startT);
+
+    std::cout << "-------------->Execution time: " << ms.count() << " (ms)" << std::endl;
+}
+
+int main()
+{
+    const unsigned n = 20000;
+
+    //Run and evaluate execution time for logic only, not an output
+    //f2 solution is faster
+    //but it requires more memory
+    //Memory is cheaper than CPU time, so
+    //having increasing in performance is more effective
+    //For n = 20000
+    //f2 Execution time: 1 (ms)
+    //f3 Execution time: 2807 (ms)
+
+    //runner(n, f1);
+    runner(n, f2);  //Execution time: 1 (ms)
+    runner(n, f3);  //Execution time: 2807 (ms)
+
+    // for (unsigned i = 0; i < 20; i++)
+    // {
+    //     const unsigned n = rand() % 25;
+
+    //     const unsigned f1Res = f1(n);
+    //     const unsigned f2Res = f2(n);
+    //     const unsigned f3Res = f3(n);
+
+    //     if (f1Res != f2Res || f1Res != f3Res)
+    //         std::cout << "----->Error!" << std::endl;
+
+    //     std::cout << "f1(" << n << ") = " << f1(n) << std::endl;
+    //     std::cout << "f2(" << n << ") = " << f2(n) << std::endl;
+    //     std::cout << "f3(" << n << ") = " << f3(n) << std::endl;
+    // }
+
+    return 0;
+}
